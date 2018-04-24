e2aa0a5f333e0dc2cf4acc8d7fda9c673ccad959
Colm Vize <colm@artomatix.com>
2018-04-24 14:02:45 +0100

    another small tweek to clBLAS package

--- cmake/projects/clBLAS/hunter.cmake
+++ cmake/projects/clBLAS/hunter.cmake
@@ -13,9 +13,9 @@ hunter_add_version(
     PACKAGE_NAME
     clBLAS
     VERSION
-    3.2
+    3.3
     URL
-	"https://github.com/Artomatix/clBLAS/archive/3.2.tar.gz"
+	"https://github.com/Artomatix/clBLAS/archive/3.3.tar.gz"
     SHA1
 	da43b20af41f2f300f1d0ab179f3a45251483cff
 )
