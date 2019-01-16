7dd0b577cd9ba17d80ce139f0f333d7f982c2516
Colm Vize <colm@artomatix.com>
2019-01-16 18:52:11 +0000

    update clBLAS version

--- cmake/projects/clBLAS/hunter.cmake
+++ cmake/projects/clBLAS/hunter.cmake
@@ -13,11 +13,11 @@ hunter_add_version(
     PACKAGE_NAME
     clBLAS
     VERSION
-    3.3
+    3.4
     URL
-	"https://github.com/Artomatix/clBLAS/archive/3.3.tar.gz"
+	"https://github.com/Artomatix/clBLAS/archive/3.4.tar.gz"
     SHA1
-	a31672191354eb44f8621fdb8ec04a7fd31f70cd
+	e4ca73f38b7c1a79bab96dd9c1ae82e76d9ca0c3
 )
 
 hunter_cmake_args(
