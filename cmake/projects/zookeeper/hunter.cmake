bb016483b4b20d1fce9552dacbe363274b6e9293
Cristina Alexa <cristina@artomatix.com>
2019-02-08 15:06:40 +0100

    "Merged hunter v0.23.93 to zookeper-test-7 mirror"

--- cmake/projects/zookeeper/hunter.cmake
+++ cmake/projects/zookeeper/hunter.cmake
@@ -6,24 +6,22 @@
 include(hunter_add_version)
 include(hunter_download)
 include(hunter_pick_scheme)
+include(hunter_cacheable)
 
 # List of versions here...
 hunter_add_version(
     PACKAGE_NAME
     zookeeper
     VERSION
-    "3.4.9-p1"
+    "3.6.0-hunter2"
     URL
-    "https://github.com/Artomatix/zookeeper/archive/3.4.9-hunter-5.tar.gz"
+    "https://github.com/Artomatix/zookeeper/archive/3.6.0-hunter-2.tar.gz"
     SHA1
-    0b239060526a65800380e82c7052a200539251f6
+    7c112a5e542883baf6d8c7039b568a2c6e4851cc
 )
 
 # Pick a download scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
 
-# Download package.
-# Two versions of library will be build by default:
-#     * libexample_A.a
-#     * libexample_Ad.a
+hunter_cacheable(zookeeper)
 hunter_download(PACKAGE_NAME zookeeper)
